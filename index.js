32c019f0ebe0f3f1ad7cca336932af5bb5cbf39a
Tazim212 <iftekhartazim17@gmail.com>
2026-05-20 23:28:24 +0600

    user verify done

--- index.js
+++ index.js
@@ -3,10 +3,42 @@ const cors = require('cors');
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
+const admin = require("firebase-admin");
 const port = process.env.PORT || 5000
+
+
+const serviceAccount = require("./smart-deals-firebase-adminsdk.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
 
 app.use(cors())
 app.use(express.json())
+
+const verifyUserToken = async (req, res, next) => {
+    if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" })
+    }
+    const token = req.headers.authorization.split(" ")[1]
+    if (!token) {
+        return res.status(401).send({ message: "unathorized access" })
+    }
+
+    try {
+        const userInfo = await admin.auth().verifyIdToken(token)
+        req.token_email = userInfo.email
+        // console.log("user info:", userInfo)
+        next()
+    }
+    catch {
+        console.log("invalid user")
+        return res.status(401).send({ message: "unathorized access" })
+    }
+}
+
+
 const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-eqifd2k-shard-00-00.tbmejyb.mongodb.net:27017,ac-eqifd2k-shard-00-01.tbmejyb.mongodb.net:27017,ac-eqifd2k-shard-00-02.tbmejyb.mongodb.net:27017/?ssl=true&replicaSet=atlas-bvjx8p-shard-0&authSource=admin&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
@@ -64,12 +96,15 @@ async function run() {
             const result = await productCollection.findOne(query)
             res.send(result)
         })
-        app.get("/myproducts/:email", async (req, res) => {
-            const email = req.params.email;
-            const query = {
-                seller_email: email
+        app.get("/myproducts", verifyUserToken, async (req, res) => {
+            const email = req.query.email;
+            const query = {}
+            if (email) {
+                if (email !== req.token_email) {
+                    return res.status(403).send({ message: "forbidden access" })
+                }
+                query.seller_email = email
             }
-            console.log(query)
             const cursor = newProductsColl.find(query)
             const result = await cursor.toArray()
             res.send(result)
@@ -102,9 +137,9 @@ async function run() {
             res.send(result)
         })
 
-        app.delete("/myproducts/:id", async(req, res) =>{
+        app.delete("/myproducts/:id", async (req, res) => {
             const id = req.params.id;
-            const query = {_id : new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await newProductsColl.deleteOne(query)
             res.send(result)
         })
@@ -126,17 +161,22 @@ async function run() {
 
         // --------------- Bids ---------------
 
-        app.get("/bids/:email", async (req, res) => {
-            const email = req.params.email
-            const query = {
-                buyer_email: email
+        app.get("/bids", verifyUserToken, async (req, res) => {
+            const email = req.query.email
+            const query = {}
+            if (email) {
+                if (email !== req.token_email) {
+                    return res.status(403).send({ message: "forbidden access" })
+                }
+                query.buyer_email = email
+
             }
             const cursor = bidCollection.find(query).sort({ bid_price: 1 })
             const result = await cursor.toArray()
             res.send(result)
         })
 
-        app.get("/product/bids/:productId", async (req, res) => {
+        app.get("/product/bids/:productId", verifyUserToken, async (req, res) => {
             const id = req.params.productId
             const query = { productId: id }
             const cursor = bidCollection.find(query)
